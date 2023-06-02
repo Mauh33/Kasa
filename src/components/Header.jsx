bcb382faa576ec13203f37cbf34ef0fe874217e2
Morgane Lejeune <morgane.lejeune@hotmail.fr>
2023-06-02 18:25:24 +0200

    Apartment-page/#6 : Dropdown and card height fixed, add underline at a tag.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import { link } from "react-router-dom";
 import Logo from "../assets/icons/logo.svg";
 
 const Header = () => {
@@ -22,34 +23,52 @@ const Header = () => {
   return (
     <header className='header'>
       <div className='logo-bloc'>
-        <img className='logo' src={Logo} alt='Logo de Kasa' />
-      </div>
-      <div className='navigation'>
         <ul>
-          <NavLink to='/' activeclassname='active'>
-            <li>
-              A
-              <span className={`${windowWidth >= 1200 ? "minify" : " "}`}>
-                CCUEIL
-              </span>
-            </li>
-          </NavLink>
-          <NavLink to='/about' activeclassname='active'>
-            <li>
-              A
-              <span
-                className={`letter-space${
-                  windowWidth >= 1200 ? " minify2" : " "
-                }`}
-              >
-                PROPOS
-              </span>{" "}
-            </li>
+          <NavLink to='/'>
+            <img className='logo' src={Logo} alt='Logo de Kasa' />
           </NavLink>
         </ul>
+      </div>
+      <div className='navigation'>
+        <NavLink
+          to='/'
+          className={({ isActive }) => (isActive ? "active" : undefined)}
+        >
+          <li>
+            A
+            <span className={`${windowWidth >= 1200 ? "minify" : " "}`}>
+              CCUEIL
+            </span>
+          </li>
+        </NavLink>
+        <NavLink
+          to='/about'
+          className={({ isActive }) => (isActive ? "active" : undefined)}
+        >
+          <li>
+            A
+            <span
+              className={`letter-space${
+                windowWidth >= 1200 ? " minify2" : " "
+              }`}
+            >
+              PROPOS
+            </span>{" "}
+          </li>
+        </NavLink>
       </div>
     </header>
   );
 };
 
 export default Header;
+
+/*
+
+classNameActive={({ isActive, isPending }) =>
+              isPending ? "unselected" : isActive ? "active-link" : ""
+            }
+
+activeClassName='active-link'
+className={isActive => (!isActive ? " unselected" : "active-link")}
+className={isActive => `nav-link${!isActive ? " unselected" : ""}`} */
